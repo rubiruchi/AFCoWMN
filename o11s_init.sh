64660d62cbbff34500dd72e389a765cb29fad2f6
Dawood Sajjadi <sajjadi@uvic.ca>
2015-03-17 12:09:24 -0700

    Create o11s_init.sh

---
+++ o11s_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PHY0=`cat /sys/class/net/wlan0/phy80211/name`
+PHY1=`cat /sys/class/net/wlan1/phy80211/name`
+PHY2=`cat /sys/class/net/wlan2/phy80211/name`
+#-------------------------
+iw wlan0 del
+iw phy $PHY0 interface add mesh0 type mp mesh_id PANDA36
+iw dev mesh0 set channel 36 HT40+
+ifconfig mesh0 up
+#-------------------------
+iw wlan1 del
+iw phy $PHY1 interface add mesh1 type mp mesh_id PANDA40
+iw dev mesh1 set channel 40 HT40+
+ifconfig mesh1 up
+#-------------------------
+iw wlan2 del
+iw phy $PHY2 interface add mesh2 type mp mesh_id PANDA6
+iw dev mesh2 set channel 6 HT40+
+ifconfig mesh2 up
